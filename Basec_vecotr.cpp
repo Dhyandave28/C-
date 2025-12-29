5269442264c3ed668d0e0d0702b47e7d33ddefb1
Dhyan Dave <homie.dave28@gmail.com>
2025-12-29 19:50:32 +0530

    Basic program using vector
    Getting familiarize with the use of vectors

---
+++ Basec_vecotr.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+  
+  std::vector<int>lemonade = {25,28,40,85,76};
+  std::vector<double>sales = {75.50,89.25,60,102,95.10};
+
+  std::cout<< lemonade[4] <<" \n";
+  std::cout<<sales[4] << " ";
+  
+}
